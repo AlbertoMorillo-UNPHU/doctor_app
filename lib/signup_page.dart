c22f86df799c367c6cb19f53f5a1e37cbb9a6a12
AlbertoMorillo-UNPHU <109552042+AlbertoMorillo-UNPHU@users.noreply.github.com>
2022-11-12 17:32:24 -0400

    Primera parte Login/Register
    Primera parte Login/Register

---
+++ lib/signup_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List images = ["g.png", "t.png", "f.png"];
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            width: w,
+            height: h * 0.37,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("img/signup.png"), fit: BoxFit.cover)),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: h * 0.18,
+                ),
+                const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 50,
+                  backgroundImage: AssetImage("img/profile1.png"),
+                )
+              ],
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              width: w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 50),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 7,
+                              offset: const Offset(1, 1),
+                              color: Colors.grey.withOpacity(0.2))
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Email",
+                          prefix: const Icon(
+                            Icons.email,
+                            color: Colors.deepOrangeAccent,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 1.0),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 7,
+                              offset: const Offset(1, 1),
+                              color: Colors.grey.withOpacity(0.2))
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Password",
+                          prefix: const Icon(
+                            Icons.password_outlined,
+                            color: Colors.deepOrangeAccent,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 1.0),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 1.0),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const SizedBox(height: 50),
+                ],
+              )),
+          const SizedBox(
+            height: 0,
+          ),
+          Container(
+            width: w * 0.5,
+            height: h * 0.08,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: const DecorationImage(
+                    image: AssetImage("img/loginbtn.png"), fit: BoxFit.cover)),
+            child: const Center(
+                child: Text("Sign up",
+                    style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white))),
+          ),
+          SizedBox(
+            height: w * 0.08,
+          ),
+          RichText(
+              text: TextSpan(
+                  text: "Sign up using one of the following methods?",
+                  style: TextStyle(color: Colors.grey[500], fontSize: 15),
+                  children: const [])),
+          Wrap(
+            children: List<Widget>.generate(3, (index) {
+              return CircleAvatar(
+                radius: 4,
+                backgroundColor: Colors.grey[500],
+                child: CircleAvatar(
+                  radius: 2,
+                  backgroundImage: AssetImage("img/${images[index]}"),
+                ),
+              );
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
